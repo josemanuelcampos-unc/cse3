116df5886252739d1c247cec91e9b54fcd42e384
josemanuelcampos-unc <68200835+josemanuelcampos-unc@users.noreply.github.com>
2020-07-12 18:04:13 -0300

    Add files via upload

---
+++ mapbox/style.js
@@ -0,0 +1,227 @@
+
+var styleJSON = {
+    "version": 8,
+    "name": "qgis2web export",
+    "pitch": 0,
+    "light": {
+        "intensity": 0.2
+    },
+    "sources": {
+        "8Convocatoria2020mapadecalor_0": {
+            "type": "geojson",
+            "data": json_8Convocatoria2020mapadecalor_0
+        }
+                    ,
+        "8Convocatoria2020_1": {
+            "type": "geojson",
+            "data": json_8Convocatoria2020_1
+        }
+                    ,
+        "7Convocatoria2020mapadecalor_2": {
+            "type": "geojson",
+            "data": json_7Convocatoria2020mapadecalor_2
+        }
+                    ,
+        "7Convocatoria2020_3": {
+            "type": "geojson",
+            "data": json_7Convocatoria2020_3
+        }
+                    ,
+        "6Convocatoria2019mapadecalor_4": {
+            "type": "geojson",
+            "data": json_6Convocatoria2019mapadecalor_4
+        }
+                    ,
+        "6Convocatoria2019_5": {
+            "type": "geojson",
+            "data": json_6Convocatoria2019_5
+        }
+                    ,
+        "5Convocatoria2019mapadecalor_6": {
+            "type": "geojson",
+            "data": json_5Convocatoria2019mapadecalor_6
+        }
+                    ,
+        "5Convocatoria2019_7": {
+            "type": "geojson",
+            "data": json_5Convocatoria2019_7
+        }
+                    ,
+        "4Convocatoria2018mapadecalor_8": {
+            "type": "geojson",
+            "data": json_4Convocatoria2018mapadecalor_8
+        }
+                    ,
+        "4Convocatoria2018_9": {
+            "type": "geojson",
+            "data": json_4Convocatoria2018_9
+        }
+                    ,
+        "3Convocatoria2018mapadecalor_10": {
+            "type": "geojson",
+            "data": json_3Convocatoria2018mapadecalor_10
+        }
+                    ,
+        "3Convocatoria2018_11": {
+            "type": "geojson",
+            "data": json_3Convocatoria2018_11
+        }
+                    ,
+        "2Convocatoria2018mapadecalor_12": {
+            "type": "geojson",
+            "data": json_2Convocatoria2018mapadecalor_12
+        }
+                    ,
+        "2Convocatoria2018_13": {
+            "type": "geojson",
+            "data": json_2Convocatoria2018_13
+        }
+                    ,
+        "1Convocatoria2018mapadecalor_14": {
+            "type": "geojson",
+            "data": json_1Convocatoria2018mapadecalor_14
+        }
+                    ,
+        "1Convocatoria2018_15": {
+            "type": "geojson",
+            "data": json_1Convocatoria2018_15
+        }
+                    ,
+        "AsociacionCivil_16": {
+            "type": "geojson",
+            "data": json_AsociacionCivil_16
+        }
+                    ,
+        "Fundacion_17": {
+            "type": "geojson",
+            "data": json_Fundacion_17
+        }
+                    ,
+        "Comercio_18": {
+            "type": "geojson",
+            "data": json_Comercio_18
+        }
+                    ,
+        "OrganismosPublicos_19": {
+            "type": "geojson",
+            "data": json_OrganismosPublicos_19
+        }
+                    ,
+        "OpenStreetMap_20": {
+            "type": "raster",
+            "tiles": ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+            "tileSize": 256
+        }},
+    "sprite": "",
+    "glyphs": "https://glfonts.lukasmartinelli.ch/fonts/{fontstack}/{range}.pbf",
+    "layers": [
+        {
+            "id": "background",
+            "type": "background",
+            "layout": {},
+            "paint": {
+                "background-color": "#ffffff"
+            }
+        },
+        {
+            "id": "lyr_8Convocatoria2020_1_0",
+            "type": "symbol",
+            "source": "8Convocatoria2020_1",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_7Convocatoria2020_3_0",
+            "type": "symbol",
+            "source": "7Convocatoria2020_3",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_6Convocatoria2019_5_0",
+            "type": "symbol",
+            "source": "6Convocatoria2019_5",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_5Convocatoria2019_7_0",
+            "type": "symbol",
+            "source": "5Convocatoria2019_7",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_4Convocatoria2018_9_0",
+            "type": "symbol",
+            "source": "4Convocatoria2018_9",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_3Convocatoria2018_11_0",
+            "type": "symbol",
+            "source": "3Convocatoria2018_11",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_2Convocatoria2018_13_0",
+            "type": "symbol",
+            "source": "2Convocatoria2018_13",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_1Convocatoria2018_15_0",
+            "type": "symbol",
+            "source": "1Convocatoria2018_15",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_AsociacionCivil_16_0",
+            "type": "symbol",
+            "source": "AsociacionCivil_16",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_Fundacion_17_0",
+            "type": "symbol",
+            "source": "Fundacion_17",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_Comercio_18_0",
+            "type": "symbol",
+            "source": "Comercio_18",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_OrganismosPublicos_19_0",
+            "type": "symbol",
+            "source": "OrganismosPublicos_19",
+            "layout": {},
+            "paint": {'icon-image': 'background_square_rounded', 'icon-rotate': 0.0}
+        }
+,
+        {
+            "id": "lyr_OpenStreetMap_20_20",
+            "type": "raster",
+            "source": "OpenStreetMap_20"
+        }],
+}
